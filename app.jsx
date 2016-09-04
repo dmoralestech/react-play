4256567c0d4a195035977d284b72e6ff9eca7ae7
darwinmorales <darwin.morales.tech@gmail.com>
2016-09-04 19:10:30 +1000

    todo list

--- app.jsx
+++ app.jsx
@@ -1,21 +1,53 @@
 /** @jsx React.DOM */
+
+
 var app = app || {};
-app.components = app.components || {};
 
 (function() {
     'use strict';
 
-    var TodoApp = app.components.TodoApp = React.createClass({
-        render: function () {
-            return React.DOM.span(null, "Interesting text goes here too..");
-        }
-    });
+    app.retrieveData = function() {
+        return app.FIXTURES;
+    };
 
     app.init = function() {
         var TodoApp = app.components.TodoApp;
-        ReactDOM.render(React.createElement(TodoApp, null ), document.getElementById("app"));
+        React.renderComponent(
+            <TodoApp />,
+            document.getElementById('app')
+        );
     };
 
     app.init();
-
 })();
+
+
+//////
+// var app = app || {};
+//
+// app.components = app.components || {};
+//
+// (function() {
+//     'use strict';
+//
+//     var TodoApp = app.components.TodoApp = React.createClass({
+//         render: function () {
+//
+//             // return React.DOM.span(null, "Interesting text goes here too..");
+//
+//             return (
+//                 <div>
+//                     <p>Hello world</p>
+//                 </div>
+//             );
+//         }
+//     });
+//
+//     app.init = function() {
+//         var TodoApp = app.components.TodoApp;
+//         ReactDOM.render(React.createElement(TodoApp, {} ), document.getElementById("app"));
+//     };
+//
+//     app.init();
+//
+// })();
---
+++ component.jsx
@@ -0,0 +1,20 @@
+/** @jsx React.DOM */
+
+var app = app || {};
+app.components = app.components || {};
+
+(function() {
+    'use strict';
+
+    var TodoApp = app.components.TodoApp = React.createClass({
+        render: function () {
+
+            return (
+                <div>
+                    <p>Hello world</p>
+                </div>
+            );
+        }
+    });
+
+})();
